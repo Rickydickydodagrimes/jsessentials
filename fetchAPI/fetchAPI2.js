fa1b26dfeb114b176040a22fe5f7062371c36194
Rickydickydodagrimes <afrasiab0981@gmail.comm>
2025-04-29 16:48:55 +0500

    fetching weather using lat and lon

---
+++ fetchAPI/fetchAPI2.js
@@ -0,0 +1,29 @@
+function showDetails(){
+    event.preventDefault();
+
+    const lat = document.getElementById('latitude').value;
+    const lon = document.getElementById('longitude').value;
+    const apiKey = '0008aa725419b10e743bdc9cfd4cea44';
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${'0008aa725419b10e743bdc9cfd4cea44'}`;
+
+    fetch(apiURL)
+    .then(response => {
+        if(!response.ok){
+            throw Error ('co ordinates not found');
+        }
+        return response.json();
+    }).then(data => {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                  <p>Temperature: ${data.main.temp} &#8451;</p>
+                                  <p>Weather: ${data.weather[0].description}</p>`;
+    }).catch(error => 
+    {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+    }
+    )
+}
+
+document.getElementById('weatherForm').addEventListener('submit', showDetails);
